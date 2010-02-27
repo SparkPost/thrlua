e5a9cff54dcdc69df7424c0df0213d22fbaf8ee9
Wez Furlong <wez@netevil.org>
2010-02-27 18:38:38 -0500

    add coverage bits from the rcpt repo

---
+++ t/strap.pm
@@ -0,0 +1,32 @@
+# vim:ts=2:sw=2:et:
+# Copyright (c) 2008-2010 Message Systems, Inc.
+package t::strap;
+use strict;
+use Test::Harness::Straps;
+
+our @ISA = qw(Test::Harness::Straps);
+
+sub _command_line {
+  my ($self, $file) = @_;
+  my $cmd;
+
+  if ($file =~ m/\.lua$/) {
+    $cmd = $ENV{LUA_EXECUTABLE} . " $file";
+  } elsif ($file =~ m/\.luat$/) {
+    $cmd = $^X . " t/doluat $file";
+  } else {
+    $cmd = $self->SUPER::_command_line($file);
+  }
+
+  if (exists $ENV{LUA_LCOV}) {
+    my $lcov = $ENV{LUA_LCOV};
+    my $infoname = $file . ".lcovinfo";
+    my $testname = $file;
+    $testname =~ s/[^a-zA-Z0-9_]/_/g;
+
+    $cmd = "$lcov -q --zerocounters --directory .; $cmd ; $lcov --capture --directory . --output-file $infoname --test-name $testname";
+  }
+  return $cmd;
+}
+
+1;
