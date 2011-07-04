78e7119ea81962e59718dca145e0d08acc79dbe2
Wez Furlong <wez@messagesystems.com>
2011-07-03 22:38:56 -0400

    reinstate the OmniTI Copyright attribution to the XML bindings.
    Adjust code to be more idiomatic and easier to follow.
    
    Add unit test (95% test coverage; remainder is runtime edges)
    
    refs #8891 (spent 3)

--- modules/xml.c
+++ modules/xml.c
@@ -7,6 +7,35 @@
  *
  * Redistribution of this material is strictly prohibited.
  *
+ * Copyright (c) 2007, OmniTI Computer Consulting, Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above
+ *       copyright notice, this list of conditions and the following
+ *       disclaimer in the documentation and/or other materials provided
+ *       with the distribution.
+ *     * Neither the name OmniTI Computer Consulting, Inc. nor the names
+ *       of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written
+ *       permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
 #include "thrlua.h"
@@ -32,38 +61,50 @@ struct xml_buffer_ptr {
   int len;
   int allocd;
 };
-static int
-xml_save_writer(void *vstr, const char *buffer, int len) {
+
+struct xpath_iter {
+  xmlXPathContextPtr ctxt;
+  xmlXPathObjectPtr pobj;
+  int cnt;
+  int idx;
+};
+
+static int xml_save_writer(void *vstr, const char *buffer, int len)
+{
   struct xml_buffer_ptr *clv = vstr;
-  if(!clv->buff) {
-    clv->allocd = 8192;
-    clv->buff = malloc(clv->allocd);
-  }
-  while(len + clv->len > clv->allocd) {
+
+  if (len + clv->len > clv->allocd) {
+    int newsize = clv->allocd ? clv->allocd * 2 : 8192;
     char *newbuff;
-    int newsize = clv->allocd;
-    newsize <<= 1;
+
+    while (newsize < len + clv->len) {
+      newsize *= 2;
+    }
+
     newbuff = realloc(clv->buff, newsize);
-    if(!newbuff) {
+    if (!newbuff) {
       return -1;
     }
+
     clv->allocd = newsize;
     clv->buff = newbuff;
   }
+
   memcpy(clv->buff + clv->len, buffer, len);
   clv->len += len;
   return len;
 }
-static int
-xml_save_closer(void *vstr) {
+
+static int xml_save_closer(void *vstr)
+{
   struct xml_buffer_ptr *clv = vstr;
   if(clv->buff == NULL) return 0;
   clv->buff[clv->len] = '\0';
   return 0;
 }
 
-char *
-xmlSaveToBuffer(xmlDocPtr doc) {
+static void xmlSaveToBuffer(lua_State *L, xmlDocPtr doc)
+{
   xmlOutputBufferPtr out;
   xmlCharEncodingHandlerPtr enc;
   struct xml_buffer_ptr buf = { NULL,0,0,0 };
@@ -72,19 +113,57 @@ xmlSaveToBuffer(xmlDocPtr doc) {
   out = xmlOutputBufferCreateIO(xml_save_writer,
                                 xml_save_closer,
                                 &buf, enc);
-  assert(out);
+
+  if (!out) {
+    luaL_error(L, "unable to create xml output buffer");
+  }
   xmlSaveFormatFileTo(out, doc, "utf8", 1);
-  return buf.buff;
+
+  lua_pushstring(L, buf.buff);
+  free(buf.buff);
 }
 
-struct xpath_iter {
-  xmlXPathContextPtr ctxt;
-  xmlXPathObjectPtr pobj;
-  int cnt;
-  int idx;
-};
-static int
-lua_xpath_iter(lua_State *L) {
+static int lua_xml_tostring(lua_State *L)
+{
+  int n;
+  xmlDocPtr *docptr = luaL_checkudata(L, 1, "xmldoc");
+
+  n = lua_gettop(L);
+  if (n != 1) luaL_error(L, "expects no arguments, got %d", n - 1);
+  xmlSaveToBuffer(L, *docptr);
+  return 1;
+}
+
+static int lua_xmlnode_tostring(lua_State *L)
+{
+  int n;
+  xmlNodePtr node = *(xmlNodePtr*)luaL_checkudata(L, 1, "xmlnode");
+  xmlOutputBufferPtr out;
+  xmlCharEncodingHandlerPtr enc;
+  struct xml_buffer_ptr buf = { NULL, 0, 0, 0 };
+
+  n = lua_gettop(L);
+  if (n != 1) luaL_error(L, "expects no arguments, got %d", n - 1);
+
+  enc = xmlGetCharEncodingHandler(XML_CHAR_ENCODING_UTF8);
+  out = xmlOutputBufferCreateIO(xml_save_writer,
+                                xml_save_closer,
+                                &buf, enc);
+  if (!out) {
+    luaL_error(L, "unable to create xml output buffer");
+  }
+  xmlNodeDumpOutput(out, node->doc, node, 0, 1, "utf8");
+  xmlOutputBufferClose(out);
+
+  lua_pushstring(L, buf.buff);
+  free(buf.buff);
+
+  return 1;
+}
+
+
+static int lua_xpath_iter(lua_State *L)
+{
   struct xpath_iter *xpi;
   xpi = lua_touserdata(L, lua_upvalueindex(1));
   if(xpi->pobj) {
@@ -101,120 +180,118 @@ lua_xpath_iter(lua_State *L) {
   }
   return 0;
 }
-static int
-lua_xpath(lua_State *L) {
+
+static int lua_xpath(lua_State *L)
+{
   int n;
   const char *xpathexpr;
-  xmlDocPtr *docptr, doc;
+  xmlDocPtr doc;
   xmlNodePtr *nodeptr = NULL;
   xmlXPathContextPtr ctxt;
   struct xpath_iter *xpi;
 
   n = lua_gettop(L);
-  /* the first arg is implicitly self (it's a method) */
-  docptr = lua_touserdata(L, lua_upvalueindex(1));
-  if(docptr != lua_touserdata(L, 1))
-    luaL_error(L, "must be called as method");
-  if(n < 2 || n > 3) luaL_error(L, "expects 1 or 2 arguments, got %d", n);
-  doc = *docptr;
-  xpathexpr = lua_tostring(L, 2);
-  if(!xpathexpr) luaL_error(L, "no xpath expression provided");
-  ctxt = xmlXPathNewContext(doc);
-  if(n == 3) {
-    nodeptr = lua_touserdata(L, 3);
-    if(nodeptr) ctxt->node = *nodeptr;
+  doc = *(xmlDocPtr*)luaL_checkudata(L, 1, "xmldoc");
+  if (n < 2 || n > 3) luaL_error(L, "expects 1 or 2 arguments, got %d", n);
+  if (n == 3) {
+    nodeptr = luaL_checkudata(L, 3, "xmlnode");
   }
-  if(!ctxt) luaL_error(L, "invalid xpath");
+
+  xpathexpr = lua_tostring(L, 2);
+  if (!xpathexpr) luaL_error(L, "no xpath expression provided");
+
+  ctxt = xmlXPathNewContext(doc);
+  if (nodeptr) ctxt->node = *nodeptr;
+  if (!ctxt) luaL_error(L, "invalid xpath");
 
   xpi = (struct xpath_iter *)lua_newuserdata(L, sizeof(*xpi));
+  memset(xpi, 0, sizeof(*xpi));
   xpi->ctxt = ctxt;
   xpi->pobj = xmlXPathEval((xmlChar *)xpathexpr, xpi->ctxt);
-  if(!xpi->pobj || xpi->pobj->type != XPATH_NODESET)
-    xpi->cnt = 0;
-  else
+  if (xpi->pobj && xpi->pobj->type == XPATH_NODESET) {
     xpi->cnt = xmlXPathNodeSetGetLength(xpi->pobj->nodesetval);
-  xpi->idx = 0;
+  }
   luaL_getmetatable(L, "xpathiter");
   lua_setmetatable(L, -2);
   lua_pushcclosure(L, lua_xpath_iter, 1);
   return 1;
 }
-static int
-lua_xmlnode_name(lua_State *L) {
-  xmlNodePtr *nodeptr;
-  /* the first arg is implicitly self (it's a method) */
-  nodeptr = lua_touserdata(L, lua_upvalueindex(1));
-  if(nodeptr != lua_touserdata(L, 1))
-    luaL_error(L, "must be called as method");
-  if(lua_gettop(L) == 1) {
-    xmlChar *v;
-    v = (xmlChar *)(*nodeptr)->name;
-    if(v) {
-      lua_pushstring(L, (const char *)v);
-    }
-    else lua_pushnil(L);
-    return 1;
+
+static int lua_xmlnode_name(lua_State *L)
+{
+  xmlNodePtr *nodeptr = luaL_checkudata(L, 1, "xmlnode");
+  xmlChar *v;
+
+  if (lua_gettop(L) != 1) {
+    luaL_error(L,"must be called with no arguments");
   }
-  luaL_error(L,"must be called with no arguments");
-  return 0;
+
+  if ((*nodeptr)->name) {
+    lua_pushstring(L, (const char *)((*nodeptr)->name));
+  } else {
+    lua_pushnil(L);
+  }
+  return 1;
 }
-static int
-lua_xmlnode_attr(lua_State *L) {
-  xmlNodePtr *nodeptr;
-  /* the first arg is implicitly self (it's a method) */
-  nodeptr = lua_touserdata(L, lua_upvalueindex(1));
-  if(nodeptr != lua_touserdata(L, 1))
-    luaL_error(L, "must be called as method");
-  if(lua_gettop(L) == 3 && lua_isstring(L,2)) {
-    const char *attr = lua_tostring(L,2);
-    if(lua_isnil(L,3))
-      xmlSetProp(*nodeptr, (xmlChar *)attr, NULL);
-    else
-      xmlSetProp(*nodeptr, (xmlChar *)attr, (xmlChar *)lua_tostring(L,3));
-    return 0;
-  }
-  if(lua_gettop(L) == 2 && lua_isstring(L,2)) {
-    xmlChar *v;
-    const char *attr = lua_tostring(L,2);
-    v = xmlGetProp(*nodeptr, (xmlChar *)attr);
-    if(v) {
-      lua_pushstring(L, (const char *)v);
-      xmlFree(v);
-    }
-    else lua_pushnil(L);
-    return 1;
+
+static int lua_xmlnode_attr(lua_State *L)
+{
+  xmlNodePtr *nodeptr = luaL_checkudata(L, 1, "xmlnode");
+  const char *attr;
+  xmlChar *v = NULL;
+
+  attr = luaL_checkstring(L, 2);
+
+  switch (lua_gettop(L)) {
+    case 3:
+      if (!lua_isnil(L, 3)) {
+        v = (xmlChar*)lua_tostring(L, 3);
+      }
+      xmlSetProp(*nodeptr, (xmlChar*)attr, v);
+      return 0;
+
+    case 2:
+      v = xmlGetProp(*nodeptr, (xmlChar*)attr);
+      if (v) {
+        lua_pushstring(L, (const char *)v);
+        xmlFree(v);
+      } else {
+        lua_pushnil(L);
+      }
+      return 1;
   }
   luaL_error(L,"must be called with one argument");
-  return 0;
 }
-static int
-lua_xmlnode_contents(lua_State *L) {
-  xmlNodePtr *nodeptr;
-  /* the first arg is implicitly self (it's a method) */
-  nodeptr = lua_touserdata(L, lua_upvalueindex(1));
-  if(nodeptr != lua_touserdata(L, 1))
-    luaL_error(L, "must be called as method");
-  if(lua_gettop(L) == 2 && lua_isstring(L,2)) {
-    const char *data = lua_tostring(L,2);
-    xmlChar *enc = xmlEncodeEntitiesReentrant((*nodeptr)->doc, (xmlChar *)data);
-    xmlNodeSetContent(*nodeptr, (xmlChar *)enc);
-    return 0;
-  }
-  if(lua_gettop(L) == 1) {
-    xmlChar *v;
-    v = xmlNodeGetContent(*nodeptr);
-    if(v) {
-      lua_pushstring(L, (const char *)v);
-      xmlFree(v);
-    }
-    else lua_pushnil(L);
-    return 1;
+
+static int lua_xmlnode_contents(lua_State *L)
+{
+  xmlNodePtr node = *(xmlNodePtr*)luaL_checkudata(L, 1, "xmlnode");
+  xmlChar *v;
+  const char *data;
+
+  switch (lua_gettop(L)) {
+    case 2:
+      data = luaL_checkstring(L, 2);
+      v = xmlEncodeEntitiesReentrant(node->doc, (xmlChar *)data);
+      xmlNodeSetContent(node, v);
+      return 0;
+    case 1:
+      v = xmlNodeGetContent(node);
+      if (v) {
+        lua_pushstring(L, (const char *)v);
+        xmlFree(v);
+      } else {
+        lua_pushnil(L);
+      }
+      return 1;
   }
   luaL_error(L,"must be called with no arguments");
   return 0;
 }
-static int
-lua_xmlnode_next(lua_State *L) {
+
+/* This is an iterator returned by node:children() */
+static int lua_xmlnode_next(lua_State *L)
+{
   xmlNodePtr *nodeptr;
   nodeptr = lua_touserdata(L, lua_upvalueindex(1));
   if(*nodeptr) {
@@ -228,31 +305,27 @@ lua_xmlnode_next(lua_State *L) {
   }
   return 0;
 }
-static int
-lua_xmlnode_addchild(lua_State *L) {
-  xmlNodePtr *nodeptr;
-  nodeptr = lua_touserdata(L, lua_upvalueindex(1));
-  if(nodeptr != lua_touserdata(L, 1))
-    luaL_error(L, "must be called as method");
-  if(lua_gettop(L) == 2 && lua_isstring(L,2)) {
-    xmlNodePtr *newnodeptr;
-    newnodeptr = (xmlNodePtr *)lua_newuserdata(L, sizeof(*nodeptr));
-    *newnodeptr = xmlNewChild(*nodeptr, NULL,
-                              (xmlChar *)lua_tostring(L,2), NULL);
-    luaL_getmetatable(L, "xmlnode");
-    lua_setmetatable(L, -2);
-    return 1;
-  }
-  luaL_error(L,"must be called with one argument");
-  return 0;
+
+static int lua_xmlnode_addchild(lua_State *L)
+{
+  xmlNodePtr *newnodeptr;
+  xmlNodePtr node = *(xmlNodePtr*)luaL_checkudata(L, 1, "xmlnode");
+  const char *v;
+
+  v = luaL_checkstring(L, 2);
+
+  newnodeptr = (xmlNodePtr *)lua_newuserdata(L, sizeof(*newnodeptr));
+  *newnodeptr = xmlNewChild(node, NULL, (xmlChar *)v, NULL);
+  luaL_getmetatable(L, "xmlnode");
+  lua_setmetatable(L, -2);
+  return 1;
 }
-static int
-lua_xmlnode_children(lua_State *L) {
-  xmlNodePtr *nodeptr, node, cnode;
-  /* the first arg is implicitly self (it's a method) */
-  nodeptr = lua_touserdata(L, lua_upvalueindex(1));
-  if(nodeptr != lua_touserdata(L, 1))
-    luaL_error(L, "must be called as method");
+
+static int lua_xmlnode_children(lua_State *L)
+{
+  xmlNodePtr node, cnode;
+  xmlNodePtr *nodeptr = luaL_checkudata(L, 1, "xmlnode");
+
   node = *nodeptr;
   cnode = node->children;
   nodeptr = lua_newuserdata(L, sizeof(cnode));
@@ -262,88 +335,39 @@ lua_xmlnode_children(lua_State *L) {
   lua_pushcclosure(L, lua_xmlnode_next, 1);
   return 1;
 }
-static int
-lua_xml_tostring(lua_State *L) {
+
+static int lua_xml_docroot(lua_State *L)
+{
   int n;
-  xmlDocPtr *docptr;
-  char *xmlstring;
-  n = lua_gettop(L);
-  /* the first arg is implicitly self (it's a method) */
-  docptr = lua_touserdata(L, lua_upvalueindex(1));
-  if(docptr != lua_touserdata(L, 1))
-    luaL_error(L, "must be called as method");
-  if(n != 1) luaL_error(L, "expects no arguments, got %d", n - 1);
-  xmlstring = xmlSaveToBuffer(*docptr);
-  lua_pushstring(L, xmlstring);
-  free(xmlstring);
-  return 1;
-}
-static int
-lua_xml_docroot(lua_State *L) {
-  int n;
-  xmlDocPtr *docptr;
+  xmlDocPtr *docptr = luaL_checkudata(L, 1, "xmldoc");
   xmlNodePtr *ptr;
+
   n = lua_gettop(L);
-  /* the first arg is implicitly self (it's a method) */
-  docptr = lua_touserdata(L, lua_upvalueindex(1));
-  if(docptr != lua_touserdata(L, 1))
-    luaL_error(L, "must be called as method");
   if(n != 1) luaL_error(L, "expects no arguments, got %d", n - 1);
+
   ptr = lua_newuserdata(L, sizeof(*ptr));
   *ptr = xmlDocGetRootElement(*docptr);
   luaL_getmetatable(L, "xmlnode");
   lua_setmetatable(L, -2);
   return 1;
 }
-static int
-lua_xpathiter_gc(lua_State *L) {
+
+static int lua_xpathiter_gc(lua_State *L)
+{
   struct xpath_iter *xpi;
   xpi = lua_touserdata(L, 1);
   xmlXPathFreeContext(xpi->ctxt);
   if(xpi->pobj) xmlXPathFreeObject(xpi->pobj);
   return 0;
 }
-static int
-xmlnode_index_func(lua_State *L) {
-  int n;
-  const char *k;
-  xmlNodePtr *udata;
-  n = lua_gettop(L); /* number of arguments */
-  assert(n == 2);
-  if(!luaL_checkudata(L, 1, "xmlnode")) {
-    luaL_error(L, "metatable error, arg1 not a xmlnode!");
-  }
-  udata = lua_touserdata(L, 1);
-  if(!lua_isstring(L, 2)) {
-    luaL_error(L, "metatable error, arg2 not a string!");
-  }
-  k = lua_tostring(L, 2);
-  switch(*k) {
-    case 'a':
-      LUA_DISPATCH(attr, lua_xmlnode_attr);
-      LUA_DISPATCH(attribute, lua_xmlnode_attr);
-      LUA_DISPATCH(addchild, lua_xmlnode_addchild);
-      break;
-    case 'c':
-      LUA_DISPATCH(children, lua_xmlnode_children);
-      LUA_DISPATCH(contents, lua_xmlnode_contents);
-      break;
-    case 'n':
-      LUA_DISPATCH(name, lua_xmlnode_name);
-      break;
-    default:
-      break;
-  }
-  luaL_error(L, "xmlnode no such element: %s", k);
-  return 0;
-}
-static int
-nl_parsexml(lua_State *L) {
+
+static int nl_parsexml(lua_State *L)
+{
   xmlDocPtr *docptr, doc;
   const char *in;
   size_t inlen;
 
-  if(lua_gettop(L) != 1) luaL_error(L, "parsexml requires one argument"); 
+  if(lua_gettop(L) != 1) luaL_error(L, "parsexml requires one argument");
 
   in = lua_tolstring(L, 1, &inlen);
   doc = xmlParseMemory(in, inlen);
@@ -352,69 +376,58 @@ nl_parsexml(lua_State *L) {
     return 1;
   }
 
-  docptr = (xmlDocPtr *)lua_newuserdata(L, sizeof(doc)); 
+  docptr = (xmlDocPtr *)lua_newuserdata(L, sizeof(doc));
   *docptr = doc;
   luaL_getmetatable(L, "xmldoc");
   lua_setmetatable(L, -2);
   return 1;
 }
-static int
-lua_xmldoc_gc(lua_State *L) {
-  xmlDocPtr *holder;
-  holder = (xmlDocPtr *)lua_touserdata(L,1);
+
+static int lua_xmldoc_gc(lua_State *L)
+{
+  xmlDocPtr *holder = luaL_checkudata(L,1, "xmldoc");
+
   xmlFreeDoc(*holder);
   return 0;
 }
-static int
-xmldoc_index_func(lua_State *L) {
-  int n;
-  const char *k;
-  xmlDocPtr *udata;
-  n = lua_gettop(L); /* number of arguments */
-  assert(n == 2);
-  if(!luaL_checkudata(L, 1, "xmldoc")) {
-    luaL_error(L, "metatable error, arg1 not a xmldoc!");
-  }
-  udata = lua_touserdata(L, 1);
-  if(!lua_isstring(L, 2)) {
-    luaL_error(L, "metatable error, arg2 not a string!");
-  }
-  k = lua_tostring(L, 2);
-  switch(*k) {
-    case 'r':
-     LUA_DISPATCH(root, lua_xml_docroot);
-     break;
-    case 't':
-     LUA_DISPATCH(tostring, lua_xml_tostring);
-     break;
-    case 'x':
-     LUA_DISPATCH(xpath, lua_xpath);
-     break;
-    default:
-     break;
-  }
-  luaL_error(L, "xmldoc no such element: %s", k);
-  return 0;
-}
 
-static const struct luaL_reg reg_libxml[] =
-{
+static const struct luaL_reg xmlnode_funcs[] = {
+  { "attr", lua_xmlnode_attr },
+  { "attribute", lua_xmlnode_attr },
+  { "addchild", lua_xmlnode_addchild },
+  { "children", lua_xmlnode_children },
+  { "contents", lua_xmlnode_contents },
+  { "name", lua_xmlnode_name },
+  { "tostring", lua_xmlnode_tostring },
+  { "__tostring", lua_xmlnode_tostring },
+  { NULL, NULL }
+};
+
+static const struct luaL_reg xmldoc_funcs[] = {
+  { "__gc", lua_xmldoc_gc },
+  { "root", lua_xml_docroot },
+  { "tostring", lua_xml_tostring },
+  { "__tostring", lua_xml_tostring },
+  { "xpath", lua_xpath },
+  { NULL, NULL }
+};
+
+static const struct luaL_reg reg_libxml[] = {
   { "parsexml", nl_parsexml },
-	{ NULL,		NULL }
+  { NULL, NULL }
 };
 
 LUALIB_API int luaopen_xml(lua_State *L)
 {
   luaL_newmetatable(L, "xmldoc");
-  lua_pushcfunction(L, lua_xmldoc_gc);
-  lua_setfield(L, -2, "__gc");
-  luaL_newmetatable(L, "xmldoc");
-  lua_pushcclosure(L, xmldoc_index_func, 0);
+  lua_pushvalue(L, -1);
   lua_setfield(L, -2, "__index");
+  luaL_register(L, NULL, xmldoc_funcs);
 
   luaL_newmetatable(L, "xmlnode");
-  lua_pushcclosure(L, xmlnode_index_func, 0);
+  lua_pushvalue(L, -1);
   lua_setfield(L, -2, "__index");
+  luaL_register(L, NULL, xmlnode_funcs);
 
   luaL_newmetatable(L, "xpathiter");
   lua_pushcfunction(L, lua_xpathiter_gc);
@@ -424,3 +437,6 @@ LUALIB_API int luaopen_xml(lua_State *L)
 
   return 1;
 }
+
+/* vim:ts=2:sw=2:et:
+ */
---
+++ t/xml.lua
@@ -0,0 +1,69 @@
+-- vim:ts=2:sw=2:et:ft=lua:
+require("Test.More");
+require("xml");
+
+plan(19);
+
+local xml_sample = [[<?xml version="1.0" encoding="utf8"?>
+<doc>
+	<item name="one">
+		<property name="prop1" value="value1">hello</property>
+	</item>
+	<item name="two">
+		<property name="prop1" value="value2">there</property>
+	</item>
+</doc>
+]]
+
+local doc = xml.parsexml(xml_sample)
+
+is(doc:tostring(), xml_sample, "tostring operates")
+is(tostring(doc), xml_sample, "__tostring operates")
+
+local root = doc:root()
+is(root:name(), "doc", "root node name is doc")
+is(root:attr("does-not-exist"), nil, "nil for non-existent attr")
+root:attr("foo", "bar")
+is(root:attr("foo"), "bar", "set foo = bar")
+
+-- counts the non-text nodes
+function count_children(node)
+	local counter = 0
+	for kid in node:children() do
+		if kid:name() != "text" then
+			counter = counter + 1
+		end
+	end
+	return counter
+end
+
+is(count_children(root), 2, "two item nodes")
+like(root:contents(), "hello", "can see hello text")
+
+local n = root:addchild("fruit");
+ok(n, "new node")
+is(n:name(), "fruit")
+like(tostring(doc), "<fruit/>", "can see fruit")
+n:contents("and vegetables");
+is(n:contents(), "and vegetables", "can see new contents")
+is(tostring(n), "<fruit>and vegetables</fruit>", "fruit and veg")
+
+for n in doc:xpath('//item[@name="one"]') do
+	is(n:name(), "item", "found an item")
+	is(n:attr("name"), "one", "it's the one we selected")
+	like(n:contents(), "hello", "can see hello in there too")
+end
+
+-- try it again with context
+for n in doc:xpath('//item[@name="one"]', root) do
+	is(n:name(), "item", "found an item under root")
+	is(n:attr("name"), "one", "it's the one we selected")
+	like(n:contents(), "hello", "can see hello in there too")
+end
+
+-- and these are for the coverage
+error_like(function ()
+	root:name("boo");
+end, "must be called with no arguments");
+
+
